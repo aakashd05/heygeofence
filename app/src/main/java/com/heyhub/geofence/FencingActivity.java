2a55c31d756fb9ad4e04b8d0abb65c01f7b7e499
akaashd <aakash@iipl.io>
2021-07-08 08:43:02 +0530

    initial commit

---
+++ app/src/main/java/com/heyhub/geofence/FencingActivity.java
@@ -0,0 +1,15 @@
+package com.heyhub.geofence;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class FencingActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fencing);
+    }
+}
